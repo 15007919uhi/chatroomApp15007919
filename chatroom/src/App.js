f9ac0e6da47d991e403468041f7327e6a373ee83
15007919uhi <15007919@uhi.ac.uk>
2021-03-12 10:50:25 +0000

    Added Chatroom.js to React app

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login.js';
+import Chatroom from './components/Chatroom.js'
 
 function App() {
   return (
     <div>
       <Login />
+      <Chatroom />
     </div>
   );
 }
