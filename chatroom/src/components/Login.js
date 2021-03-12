b8def0319e5376e91940be3667a0952afa85d254
15007919uhi <15007919@uhi.ac.uk>
2021-03-12 12:07:15 +0000

    Updated App, Index and Login files for React app

--- chatroom/src/components/Login.js
+++ chatroom/src/components/Login.js
@@ -1,8 +1,32 @@
+import firebase from "firebase/app";
+import "firebase/auth";
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div style={{ display: 'flex', flex: 1, height: '100vh' }}>
+            <div style={{
+                display: 'flex',
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                minWidth: 300
+            }}>
+                <button style={{
+                    backgroundColor: 'red',
+                    borderWidth: 0,
+                    paddingTop: 12,
+                    paddingBottom: 12,
+                    paddingLeft: 24,
+                    paddingRight: 24,
+                    color: 'white',
+                    fontSize: 18,
+                    fontWeight: 'bold'
+                }} onClick={async () => {
+                    const provider = new firebase.auth.GoogleAuthProvider();
+                    const googleLogin = await firebase.auth().signInWithPopup(provider);
+                    console.log(googleLogin)
+                }}>Login with Google</button>
+            </div>
         </div>
     );
 }
--- chatroom/src/index.js
+++ chatroom/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+import 'firebase/analytics'
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCJHZ6Wq9hU-NO2jqO8fVg9ajq-Z2NV3uE",
+    authDomain: "chatroomapp15007919-f739f.firebaseapp.com",
+    projectId: "chatroomapp15007919-f739f",
+    storageBucket: "chatroomapp15007919-f739f.appspot.com",
+    messagingSenderId: "354855475233",
+    appId: "1:354855475233:web:a80daea26347c56b6bab60",
+    measurementId: "G-M4533PEW2N"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
